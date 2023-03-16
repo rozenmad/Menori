ce489f6625350fa0848d134b0d57fb5711b64806
rozenmad <rozenmad@gmail.com>
2023-03-16 14:44:50 +0300

    upd

--- menori/modules/shaders/chunks/skinning_vertex_base.glsl
+++ menori/modules/shaders/chunks/skinning_vertex_base.glsl
@@ -2,10 +2,10 @@
       uniform samplerBuffer joints_matrices_buffer;
 
       mat4 getBoneMatrix(float i) {
-            vec4 v1 = texelFetch(joints_matrices_buffer, int(float(i)*0xFFFF)*4+0);
-            vec4 v2 = texelFetch(joints_matrices_buffer, int(float(i)*0xFFFF)*4+1);
-            vec4 v3 = texelFetch(joints_matrices_buffer, int(float(i)*0xFFFF)*4+2);
-            vec4 v4 = texelFetch(joints_matrices_buffer, int(float(i)*0xFFFF)*4+3);
+            vec4 v1 = texelFetch(joints_matrices_buffer, int(i*float(0xFFFF))*4+0);
+            vec4 v2 = texelFetch(joints_matrices_buffer, int(i*float(0xFFFF))*4+1);
+            vec4 v3 = texelFetch(joints_matrices_buffer, int(i*float(0xFFFF))*4+2);
+            vec4 v4 = texelFetch(joints_matrices_buffer, int(i*float(0xFFFF))*4+3);
             mat4 bone = mat4( v1, v2, v3, v4 );
             return bone;
       }
